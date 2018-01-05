1e692d3d3e05eb0859254f56a62093417ae20732
Adam Yang <hanbyang@microsoft.com>
2018-01-05 12:57:33 -0800

    Added triangle graph

--- lib/HLSL/PhiToSelect.cpp
+++ lib/HLSL/PhiToSelect.cpp
@@ -3,17 +3,20 @@
 #include "llvm/Analysis/DivergenceAnalysis.h"
 #include "llvm/Pass.h"
 #include "llvm/IR/Function.h"
+#include "llvm/IR/Module.h"
 #include "llvm/IR/BasicBlock.h"
 #include "llvm/IR/Instruction.h"
 #include "llvm/IR/Instructions.h"
 
 #include "dxc/HLSL/DxilGenerationPass.h"
+#include "dxc/HLSL/DxilModule.h"
 
 using namespace llvm;
 
 class PhiToSelect : public FunctionPass
 {
   DivergenceAnalysis *DA = nullptr;
+  Module *M = nullptr;
 public:
   static char ID;
   PhiToSelect() : FunctionPass(ID) {}
@@ -28,6 +31,14 @@ void PhiToSelect::getAnalysisUsage(AnalysisUsage &AU) const {
   AU.addRequired<DivergenceAnalysis>();
 }
 
+static bool HintToPreserveCF(Module *M, BranchInst *Br) {
+  return false;
+  if (Br->hasMetadataOtherThanDebugLoc()) {
+    return true;
+  }
+  return false;
+}
+
 static BasicBlock *GetCommonSinglePredecessorAndCond(BasicBlock *A, BasicBlock *B, Value **cond, BranchInst **Br) {
   *cond = nullptr;
   *Br = nullptr;
@@ -38,15 +49,25 @@ static BasicBlock *GetCommonSinglePredecessorAndCond(BasicBlock *A, BasicBlock *
   auto AP = A->getSinglePredecessor();
   auto BP = B->getSinglePredecessor();
 
-  if (!AP || AP != BP) {
-    return nullptr;
+  BasicBlock *TopBB = nullptr;
+  if (AP == B) {
+    TopBB = B;
+  }
+  else if (BP == A) {
+    TopBB = A;
+  }
+  else {
+    if (!AP || AP != BP) {
+      return nullptr;
+    }
+    TopBB = AP;
   }
 
-  if (auto BranchI = dyn_cast<BranchInst>(AP->getTerminator())) {
+  if (auto BranchI = dyn_cast<BranchInst>(TopBB->getTerminator())) {
     if (BranchI->isConditional()) {
       *cond = BranchI->getCondition();
       *Br = BranchI;
-      return AP;
+      return TopBB;
     }
   }
 
@@ -102,15 +123,26 @@ static void MoveAllInstsTo(BasicBlock *BB, Instruction *InsertPoint) {
   }
 }
 
+// Diamond:
+//    Top
+//   /   |
+// B[0] B[1]
+//   |  /
+//    BB
 template<unsigned N>
-static bool TryMergeCandidate(BasicBlock &BB, MergeCandidate &Candidate, SmallVector<PHINode *, N> &Phis) {
+bool TryMergeDiamond(BasicBlock &BB, MergeCandidate &Candidate, SmallVector<PHINode *, N> &Phis) {
+  auto TopBB = Candidate.Br->getParent();
+
+  if (Candidate.BB[0]->getSingleSuccessor() != &BB || Candidate.BB[1]->getSingleSuccessor() != &BB) {
+    return false;
+  }
+
   SmallPtrSet<Instruction *, 5> Insts;
   for (auto PN : Phis) {
     if (!CheckDom(PN->getIncomingValueForBlock(Candidate.BB[0]), Candidate.BB[0], Insts) ||
-        !CheckDom(PN->getIncomingValueForBlock(Candidate.BB[1]), Candidate.BB[1], Insts))
+      !CheckDom(PN->getIncomingValueForBlock(Candidate.BB[1]), Candidate.BB[1], Insts))
     {
       return false;
-      break;
     }
   }
 
@@ -118,11 +150,6 @@ static bool TryMergeCandidate(BasicBlock &BB, MergeCandidate &Candidate, SmallVe
     return false;
   }
 
-  if (Candidate.BB[0]->getSingleSuccessor() != &BB || Candidate.BB[0]->getSingleSuccessor() != &BB) {
-    return false;
-  }
-
-  auto TopBB = Candidate.Br->getParent();
   MoveAllInstsTo(Candidate.BB[0], TopBB->getTerminator());
   MoveAllInstsTo(Candidate.BB[1], TopBB->getTerminator());
 
@@ -147,8 +174,70 @@ static bool TryMergeCandidate(BasicBlock &BB, MergeCandidate &Candidate, SmallVe
   return true;
 }
 
+// Triangle:
+//    Top _ 
+//    |    MidBB
+//    |   /
+//    |  /
+//    BB
+template<unsigned N>
+bool TryMergeTriangle(BasicBlock &BB, MergeCandidate &Candidate, SmallVector<PHINode *, N> &Phis) {
+  auto TopBB = Candidate.Br->getParent();
+  auto MidBB = Candidate.BB[0] == TopBB ? Candidate.BB[1] : Candidate.BB[0];
+  
+
+  if (MidBB->getSingleSuccessor() != &BB) {
+    return false;
+  }
+
+  SmallPtrSet<Instruction *, 5> Insts;
+  for (PHINode *PN : Phis) {
+    if (!CheckDom(PN->getIncomingValueForBlock(MidBB), MidBB, Insts)) {
+      return false;
+    }
+  }
+
+  if (HasInstructionsMissing(MidBB, Insts)) {
+    return false;
+  }
+
+  MoveAllInstsTo(MidBB, TopBB->getTerminator());
+
+  for (PHINode *PN : Phis) {
+    Value *IV[] = {
+      PN->getIncomingValueForBlock(TopBB),
+      PN->getIncomingValueForBlock(MidBB),
+    };
+
+    PN->removeIncomingValue(TopBB);
+    auto Sel = SelectInst::Create(Candidate.Cond, IV[0], IV[1], "", TopBB->getTerminator());
+    PN->addIncoming(Sel, TopBB);
+
+    PN->removeIncomingValue(MidBB);
+  }
+
+  BranchInst::Create(&BB, TopBB->getTerminator());
+  TopBB->getTerminator()->eraseFromParent();
+
+  MidBB->eraseFromParent();
+
+  return true;
+}
+
+template<unsigned N>
+static bool TryMergeCandidate(BasicBlock &BB, MergeCandidate &Candidate, SmallVector<PHINode *, N> &Phis) {
+  auto TopBB = Candidate.Br->getParent();
+  if (TopBB == Candidate.BB[0] || TopBB == Candidate.BB[1]) {
+    return TryMergeTriangle(BB, Candidate, Phis);
+  }
+  else {
+    return TryMergeDiamond(BB, Candidate, Phis);
+  }
+  return false;
+}
+
 bool PhiToSelect::DoBasicBlock(BasicBlock &BB) {
-  SmallVector<PHINode *, 6> Phis;
+  SmallVector<PHINode *, 32> Phis;
 
   for (Instruction &I : BB) {
     if (auto PN = dyn_cast<PHINode>(&I)) {
@@ -175,7 +264,7 @@ bool PhiToSelect::DoBasicBlock(BasicBlock &BB) {
       auto BB2 = PN->getIncomingBlock(j);
       MergeCandidate Candidate;
       if (GetCommonSinglePredecessorAndCond(BB1, BB2, &Candidate.Cond, &Candidate.Br)) {
-        if (DA->isDivergent(Candidate.Cond)) {
+        if (DA->isDivergent(Candidate.Cond) && !HintToPreserveCF(M, Candidate.Br)) {
           Candidate.BB[0] = BB1;
           Candidate.BB[1] = BB2;
           Candidates.push_back(Candidate);
@@ -194,6 +283,7 @@ bool PhiToSelect::DoBasicBlock(BasicBlock &BB) {
 
 bool PhiToSelect::runOnFunction(Function &F) {
 
+  M = F.getParent();
   DA = &getAnalysis<DivergenceAnalysis>();
   bool Changed = false;
   for (auto &FI : F) {
